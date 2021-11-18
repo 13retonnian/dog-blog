615366ec57d66bc60b26abd139a6c53fc50d3567
Ashlyn <lilyx13@tutanota.com>
2021-11-18 12:04:35 -0700

    added blog content

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -2,19 +2,20 @@
   <article>
     <h2>{{ post.title }}</h2>
     <nuxt-content :document="post" />
+    <p>This is easy once you get the hang of it.</p>
   </article>
 </template>
 <script>
 export default {
   async asyncData({ $content, params, error }) {
     try {
-      const post = await $content(`post/${params.slug}`).fetch()
+      const post = await $content(`blog/${params.slug}`).fetch()
+      return {
+        post,
+      }
     } catch (error) {
-      error({ message: 'Post Not Found' })
+      error('No article found')
     }
-  },
-  return: {
-    post,
   },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,26 @@
 <template>
- 
+  <div>
+    <section>
+      <h2>Articles</h2>
+      <nav>
+        <ul>
+          <li v-for="post in posts" :key="post.slug">
+            <NuxtLink :to="post.slug"> {{ post.title }}</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  </div>
 </template>
 <script>
 export default {
+  // fetch the data from the blog in the content folder
+  async asyncData({ $content }) {
+    const posts = await $content('blog').fetch()
+    return {
+      posts,
+    }
+  },
   head() {
     return {
       script: [
